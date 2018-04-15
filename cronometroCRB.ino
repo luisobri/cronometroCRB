b8ac8ede094584f8eccd28152b5eb856f276d46b
luisesn <luisyo2001@gmail.com>
2018-04-15 15:40:42 +0200

    Mejoras
    No usar la interrupcion, solo el timer para calcular las cosas

--- cronometroCRB.ino
+++ cronometroCRB.ino
@@ -3,17 +3,14 @@
 #include <MultiFuncShield.h>
 
 int LDR;
-volatile unsigned int clockMilliSeconds = 0;
-volatile byte clockCentiSeconds = 0;
-volatile byte clockSeconds = 0;
-bool coche = false;
+bool haEmpezado = false;
 int pasada = 0;
 
 static byte LDR_MIN = 180;
 static byte numeroVueltas = 1; // Numero de vueltas que contaremos
-int vuelta[4];
-int total = 0;
-int masRapida = 99999;
+unsigned int vuelta[4];
+unsigned int total = 0;
+unsigned int masRapida = 99999;
 
 boolean isr = false;
 boolean isrC = false;
@@ -21,7 +18,7 @@ boolean isrC = false;
 void setup()
 {
   Timer1.initialize(1000); // Inicializamos Timer1 a mil microsegundos -> 1 milisegundo
-  MFS.userInterrupt = clockISR;
+  //MFS.userInterrupt = clockISR; // No lo usamos para nada
   MFS.initialize(&Timer1);
   MFS.blinkDisplay(DIGIT_ALL);
   MFS.beep(10);
@@ -30,34 +27,38 @@ void setup()
 
 void loop()
 {
-  if (isr) {
-    ejecutar_isr();
-  }
-  if (isrC) {
-    displayTime(clockSeconds, clockCentiSeconds);
-    isrC=false;
-  }
+  char time[5];
+  unsigned long m=millis();
+  comprobarPaso(m);
+  // Imprimimos el tiempo
+  sprintf(time, "%04d", m/100);
+  MFS.write(time, 1);
 }
 
-void ejecutar_isr() {
+
+// Pasamos como parametro los millis para ahorrar la llamada al funcion
+void comprobarPaso(unsigned long m) {
+  unsigned int tmp = 0;
+  char time[5];
+  
   LDR = analogRead(A5);
   if (LDR < LDR_MIN) { // Haz cortado
-    if (coche) { // Ya ha arrancado la carrera
-      vuelta[pasada] = clockSeconds * 100 + clockCentiSeconds;
-      clockMilliSeconds = 0;
-      clockCentiSeconds = 0;
-      clockSeconds = 0;
+    if (haEmpezado) { // Ya ha arrancado la carrera
+      vuelta[pasada] = m;
       MFS.beep(10);
-      char time[5];
+      // Imprimimos el número de vuelta
       sprintf(time, "%04d", pasada);
       MFS.write(time, 1);
       delay(500);
       if (pasada == numeroVueltas) { // SE ACABO
         total = 0;
-        for (int i = 0; i < numeroVueltas; i++) {
-          total += vuelta[i];
-          if (masRapida>vuelta[i]) masRapida=vuelta[i];
+        // Calculamos el total y la vuelta mas rapida
+        for (unsigned int i = 1; i <= numeroVueltas; i++) {
+          tmp=vuelta[i]-vuelta[i-1]; // Calculamos el tiempo de vuelta
+          total += tmp;
+          if (masRapida>tmp) masRapida=tmp;
         }
+        // Ponemos los datos en bucle
         while (1) {
           MFS.write("Fin");
           delay(1000);
@@ -72,55 +73,12 @@ void ejecutar_isr() {
           MFS.write(time, 1);
           delay(1000);
         }
-      }
-      pasada++;
+      }      
     } else {
       MFS.blinkDisplay(DIGIT_ALL, 0);
-      coche = true; // ¡Arrancamos la carrera!
-      // Ponemos a cero el reloj
-      clockMilliSeconds = 0;
-      clockCentiSeconds = 0;
-      clockSeconds = 0;
-      pasada = 0;
-    }
-  } else {
-    if (coche) { // Ya ha arrancado la carrera
-      //MFS.beep(20, 2, 8);
-    } else {
-      clockMilliSeconds = 0;
-      clockCentiSeconds = 0;
-      clockSeconds = 0;
-    }
-  }
-
-  isr = false;
-}
-
-//---------------------------------------------------------
-void displayTime (byte seconds, byte centiseconds)
-{
-  char time[5];
-  sprintf(time, "%04d", (seconds * 100) + centiseconds);
-  MFS.write(time, 1);
-}
-
-//--------------------------------------------------------------------------------------------------
-void clockISR ()
-{
-  isr = true;
-  clockMilliSeconds++;
-  if (clockMilliSeconds >= 10) {
-    clockMilliSeconds = 0;
-    clockCentiSeconds++;
-    isrC = true;
-    if (clockCentiSeconds >= 100) {
-      clockCentiSeconds = 0;
-      clockSeconds++;
-      if (clockSeconds >= 100) {
-        clockCentiSeconds = 0;
-        clockCentiSeconds = 0;
-        clockSeconds = 0;
-      }
+      haEmpezado = true; // ¡Arrancamos la carrera!
+      vuelta[0]=m; // Inicio de la carrera
+      pasada++;
     }
   }
 }
