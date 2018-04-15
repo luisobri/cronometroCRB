d7efd309b169a705c013658454509c23abae18f8
luisesn <luisyo2001@gmail.com>
2018-04-15 15:23:53 +0200

    Mejoras y adaptación a CBot

--- cronometroCRB.ino
+++ cronometroCRB.ino
@@ -2,19 +2,21 @@
 #include <Wire.h>
 #include <MultiFuncShield.h>
 
-volatile int LDR;
+int LDR;
 volatile unsigned int clockMilliSeconds = 0;
 volatile byte clockCentiSeconds = 0;
 volatile byte clockSeconds = 0;
-volatile bool coche = false;
-volatile int pasada = 0;
+bool coche = false;
+int pasada = 0;
 
 static byte LDR_MIN = 180;
-static byte numeroVueltas = 4;
-volatile int vuelta[15];
-volatile int total = 0;
+static byte numeroVueltas = 1; // Numero de vueltas que contaremos
+int vuelta[4];
+int total = 0;
+int masRapida = 99999;
 
 boolean isr = false;
+boolean isrC = false;
 
 void setup()
 {
@@ -23,13 +25,18 @@ void setup()
   MFS.initialize(&Timer1);
   MFS.blinkDisplay(DIGIT_ALL);
   MFS.beep(10);
-  Serial.begin(9600);
+  Serial.begin(115200); // Mas velocidad -> menos tiempo para escribir por el puerto serie
 }
 
 void loop()
 {
-  displayTime(clockSeconds, clockCentiSeconds);
-  if (isr) ejecutar_isr();
+  if (isr) {
+    ejecutar_isr();
+  }
+  if (isrC) {
+    displayTime(clockSeconds, clockCentiSeconds);
+    isrC=false;
+  }
 }
 
 void ejecutar_isr() {
@@ -40,27 +47,40 @@ void ejecutar_isr() {
       clockMilliSeconds = 0;
       clockCentiSeconds = 0;
       clockSeconds = 0;
-
       MFS.beep(10);
       char time[5];
       sprintf(time, "%04d", pasada);
       MFS.write(time, 1);
       delay(500);
-      pasada++;
       if (pasada == numeroVueltas) { // SE ACABO
-        total=0;
-        for (int i = 0; i < numeroVueltas; i++) total += vuelta[i];
-        sprintf(time, "%04d", total);
+        total = 0;
+        for (int i = 0; i < numeroVueltas; i++) {
+          total += vuelta[i];
+          if (masRapida>vuelta[i]) masRapida=vuelta[i];
+        }
         while (1) {
           MFS.write("Fin");
           delay(1000);
+          MFS.write("Tot");
+          delay(500);
+          sprintf(time, "%04d", total);
+          MFS.write(time, 1);
+          delay(1000);
+          MFS.write("Rap");
+          delay(500);
+          sprintf(time, "%04d", masRapida);
           MFS.write(time, 1);
           delay(1000);
         }
       }
+      pasada++;
     } else {
-      MFS.blinkDisplay(DIGIT_ALL,0);
-      coche = true; // Arrancamos la carrera!
+      MFS.blinkDisplay(DIGIT_ALL, 0);
+      coche = true; // ¡Arrancamos la carrera!
+      // Ponemos a cero el reloj
+      clockMilliSeconds = 0;
+      clockCentiSeconds = 0;
+      clockSeconds = 0;
       pasada = 0;
     }
   } else {
@@ -88,27 +108,18 @@ void displayTime (byte seconds, byte centiseconds)
 void clockISR ()
 {
   isr = true;
-  if (coche == true) // Si hay coche sumamos un milisegundo al reloj
-  {
-    clockMilliSeconds++;
-    if (clockMilliSeconds >= 10)
-    {
-      clockMilliSeconds = 0;
-      clockCentiSeconds++;
-
-      if (clockCentiSeconds >= 100)
-      {
+  clockMilliSeconds++;
+  if (clockMilliSeconds >= 10) {
+    clockMilliSeconds = 0;
+    clockCentiSeconds++;
+    isrC = true;
+    if (clockCentiSeconds >= 100) {
+      clockCentiSeconds = 0;
+      clockSeconds++;
+      if (clockSeconds >= 100) {
         clockCentiSeconds = 0;
-        clockSeconds++;
-        Serial.println(coche);
-        Serial.println(LDR);
-
-        if (clockSeconds >= 100)
-        {
-          clockCentiSeconds = 0;
-          clockCentiSeconds = 0;
-          clockSeconds = 0;
-        }
+        clockCentiSeconds = 0;
+        clockSeconds = 0;
       }
     }
   }
